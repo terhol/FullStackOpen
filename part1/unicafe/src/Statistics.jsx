b162e79b98d3edde7d1a00145f9428eabc1aaa2b
Tereza Holm <holm.tereza@gmail.com>
2024-03-14 12:13:46 +0100

    finished unicafe step 2

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,9 +1,17 @@
 export const Statistics = ({good, neutral, bad}) => {
+
+    const total = good + neutral + bad
+    const averageScore = (good - bad) / total
+    const positiveFeedback = (good / total) * 100
+    
     return (
         <>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {total>0 ? averageScore : 0}</p>
+        <p>positive {total>0 ? positiveFeedback : 0} %</p>
         </>
     )
 }
