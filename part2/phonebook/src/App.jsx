065ec1c42bbe8e5c3ccb9e865083e6723c370707
Tereza Holm <holm.tereza@gmail.com>
2024-03-15 15:46:54 +0100

    Finished phonebook step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,10 +6,17 @@ function App() {
  const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
  const [newName, setNewName] = useState('')
 
+ const allNames = persons.map(person => person.name)
+
  const addPerson = (event) => {
   event.preventDefault()
   const newPerson = {name: newName}
+  if(allNames.includes(newName)){
+    alert(`${newName} is already added to phonebook.`)
+  }
+  else {
   setPersons(persons.concat(newPerson))
+  }
   setNewName('')
  } 
 
