f98bf7ec31176a84896e6e2b4d57a1bdf44a09aa
Tereza Holm <holm.tereza@gmail.com>
2024-03-27 16:03:43 +0100

    Finished phonebook be step 7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 app.use(express.json())
+app.use(morgan('tiny'))
 
 const PORT = 3001
 const baseURL = '/api/persons'
