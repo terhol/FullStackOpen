42802da18979b436f32152f33c365a8483633ac1
Tereza Holm <holm.tereza@gmail.com>
2024-05-08 14:26:47 +0200

    Added full info about blogs

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,24 @@
-const Blog = ({ blog }) => (
-  <div>
-    <a href={blog.url}>{blog.title}</a> {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    marginRight: 900,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    borderRadius: 10,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <a href={blog.url}>{blog.title}</a> by {blog.author}
+      <br />
+      {blog.url}
+      <br />
+      {blog.likes} likes &nbsp;
+      <button className="pure-button pure-button-primary">Like </button>
+    </div>
+  )
+}
 
 export default Blog
